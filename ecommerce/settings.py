1c97fe622282e128ed9e5432d622949702f4e6c6
uma manikanta <umamanikanta1477@gmail.com>
2026-06-09 11:09:27 +0530

    Updated ALLOWED_HOSTS

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dj@d$&w*x5663$e6((xzksi6=+!x#+k@+#*soa_i5a7844w(=-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
